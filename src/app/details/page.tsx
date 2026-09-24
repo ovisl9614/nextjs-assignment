7f69b77f00e624ae57e95fb9d8f711f463827af1
ovisl9614 <ovihas12@gmail.com>
2026-09-24 20:35:30 +0600

    first commit

---
+++ src/app/details/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DetailsPage = () => {
+    return (
+        <div>
+            <h2>
+                Details Page is Cooking
+            </h2>
+        </div>
+    );
+};
+
+export default DetailsPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,22 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Oswald, Inter } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
+
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const oswald = Oswald({
+  variable: "--font-oswald",
   subsets: ["latin"],
 });
 
@@ -21,9 +29,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h2 className={`${oswald.className} font-bold text-white text-2xl bg-black`}>FITLOG</h2>
+        {children}
+        <h2 className={`${oswald.className} font-bold text-white text-2xl bg-black`}>FITLOG</h2>
+
+        </body>
     </html>
   );
 }
